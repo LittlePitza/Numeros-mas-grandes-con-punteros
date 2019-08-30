a35b2655bd8a1a24dcdb551c098fe7360522f077
PeTskyMiranda <luishc888@gmail.com>
2019-08-30 12:34:22 -0500

    Arreglos Menores a Bugs

--- Practica 2.cpp
+++ Practica 2.cpp
@@ -1,25 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-int arreglo[10];
 
-int main(){
-	
-	int mayor = INT_MIN;
-	int *puntero = arreglo;
-	for (;puntero<=&arreglo[9];puntero++){
-		cout<<"Escribe un numero entero ";
-		cin>>*puntero;
-		if(*puntero>mayor) mayor=*puntero;
-	}
-	cout<<"El numero mas grande es"<<;
-	return 0;
-	
-	
-	
-	
-	
-	
-	
-	
-	return 0;
+struct direccion{
+char calle[20];
+int cp;
+char ciudad[20];
+};
+
+struct persona{
+char nombre[100];
+int edad;
+float estatura;
+struct direccion dire;
+};
+
+int main(int argc, char** argv){
+struct persona P1[5];
+struct persona *apuntador;
+
+apuntador=&P1[0];
+
+for(int i=0;i<1;i++){
+    cout<<"Nombre: ";
+    cin.getline(P1[i].nombre,100,'\n');
+    cout<<"Edad: ";
+    cin>>P1[i].edad;
+    cout<<"Estatura; ";
+    cin>>P1[i].estatura;
+    cout<<" Calle: ";
+    fflush(stdin);
+    cin.getline(P1[i].dire.calle,20,'\n');
+
+}
+
+//
+
+cout<<endl<<"Nombre guardado: "<<P1[0].nombre;
+cout<<endl<<"Edad guardad: "<<P1[0].edad;
+cout<<endl<<"Estatura Guardada:"<<P1[0].estatura;
+cout<<endl<<"Calle Guardada:"<<P1[0].dire.calle<<"\n";
+
+system("pause");
+return 0;
+
 }
